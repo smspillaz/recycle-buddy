2c3c0e552c79de88d1d29408b4cbb32c8d213aa3
Sam Spilsbury <smspillaz@gmail.com>
2023-11-11 13:29:01 +0200

    view: Simple flask app

---
+++ view.py
@@ -0,0 +1,26 @@
+import asyncio
+from flask import Flask
+import os
+from vision import RECYCLING_STREAMS, KIERRATYS_MATERIAL_TYPES, encode_image
+
+app = Flask(__name__)
+
+
+async def async_get_data():
+    await asyncio.sleep(1)
+    return 'Done!'
+
+
+@app.route("/api/vision")
+async def get_data():
+    data = await async_get_data()
+    return data
+
+
+if __name__ == "__main__":
+    # OpenAI API Key
+    with open("api_key.txt") as f:
+        api_key = f.read().strip()
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
