cdee18731a45bb2dd4bfcfadf326be87fdf15eff
linhnguyen222 <nguyenmylinh.9720@gmail.com>
2023-11-11 21:32:41 +0200

    web ui

--- ui/src/ImageUpload.js
+++ ui/src/ImageUpload.js
@@ -1,37 +1,92 @@
-// ImageUpload.js
 import React, { useState } from 'react';
-import axios from 'axios';
+import {
+  Button,
+  CircularProgress,
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  TextField,
+} from '@mui/material';
+import './ImageUpload.css'; // Import a CSS file for additional styling
 
 const ImageUpload = () => {
   const [selectedImage, setSelectedImage] = useState(null);
-  const [response, setResponse] = useState(null);
+  const [base64Image, setBase64Image] = useState(null);
+  const [responseMessage, setResponseMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const handleImageChange = (e) => {
+  const handleImageChange = async (e) => {
     const file = e.target.files[0];
-    setSelectedImage(file);
-  };
 
-  const handleImageUpload = async () => {
-    const formData = new FormData();
-    formData.append('image', selectedImage);
+    // Check if a file is selected
+    if (file) {
+      setLoading(true);
 
-    try {
-      const response = await axios.post('YOUR_API_ENDPOINT', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      setResponse(response.data);
-    } catch (error) {
-      console.error('Error uploading image:', error);
+      const reader = new FileReader();
+
+      // Set up the FileReader callback
+      reader.onloadend = async () => {
+        // The result property contains the data as a base64 encoded string
+        const base64Result = reader.result;
+        setBase64Image(base64Result);
+
+        // Send base64 image to the server
+        try {
+          const response = await fetch('YOUR_API_ENDPOINT', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ base64Image: base64Result }),
+          });
+
+          const responseData = await response.json();
+          setResponseMessage(responseData.message);
+        } catch (error) {
+          console.error('Error uploading image:', error);
+          setResponseMessage('Error uploading image. Please try again.');
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      // Read the file as a data URL, triggering the onloadend callback
+      reader.readAsDataURL(file);
+
+      // Set the selected image for display
+      setSelectedImage(URL.createObjectURL(file));
     }
   };
 
   return (
     <div>
-      <input type="file" onChange={handleImageChange} />
-      <button onClick={handleImageUpload}>Upload Image</button>
-      {response && <div>API Response: {JSON.stringify(response)}</div>}
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6">Uncle Sam and eco</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md" style={{ marginTop: '20px' }}>
+        <div className="image-upload-container">
+          <TextField
+            type="file"
+            label="Choose Image"
+            onChange={handleImageChange}
+            InputLabelProps={{ shrink: true }}
+          />
+          {selectedImage && (
+            <div className="image-preview">
+              <h3>Selected Image:</h3>
+              <img src={selectedImage} alt="Selected" />
+              {loading ? (
+                <CircularProgress style={{ marginTop: 10 }} />
+              ) : (
+                responseMessage && <p>{responseMessage}</p>
+              )}
+            </div>
+          )}
+        </div>
+      </Container>
     </div>
   );
 };
