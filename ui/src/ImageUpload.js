7b0f86f71eca7db02869ab9ca411b407d67658de
Sam Spilsbury <smspillaz@gmail.com>
2023-11-11 23:50:27 +0200

    ui: Add dispose mode

--- ui/src/ImageUpload.js
+++ ui/src/ImageUpload.js
@@ -7,6 +7,8 @@ import {
   Typography,
   Container,
   TextField,
+  ToggleButton,
+  ToggleButtonGroup
 } from '@mui/material';
 import './ImageUpload.css'; // Import a CSS file for additional styling
 
@@ -14,6 +16,7 @@ const ImageUpload = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [responseError, setResponseError] = useState(null);
   const [responseStream, setResponseStream] = useState(null);
+  const [disposeMode, setDisposeMode] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const handleImageChange = async (e) => {
@@ -21,11 +24,22 @@ const ImageUpload = () => {
 
     // Check if a file is selected
     if (file) {
+      // Set the selected image for display
+      setSelectedImage({
+          url: URL.createObjectURL(file),
+          file
+      });
+    }
+  };
+
+  const handleDisposeModeChange = async (e, mode) => {
+    setDisposeMode(mode);
+    if (selectedImage.file) {
       setLoading(true);
 
       // The result property contains the data as a base64 encoded string
       let data = new FormData();
-      data.append('img', file);
+      data.append('img', selectedImage.file);
 
       // Send base64 image to the server
       try {
@@ -45,11 +59,8 @@ const ImageUpload = () => {
       } finally {
         setLoading(false);
       }
-
-      // Set the selected image for display
-      setSelectedImage(URL.createObjectURL(file));
     }
-  };
+  }
 
   return (
     <div>
@@ -67,36 +78,52 @@ const ImageUpload = () => {
             InputLabelProps={{ shrink: true }}
           />
           {selectedImage && (
-            <div className="image-preview">
-              <h3>Selected Image:</h3>
-              <img src={selectedImage} alt="Selected" />
-              {loading ? (
-                <CircularProgress style={{ marginTop: 10 }} />
-              ) : (
-                <div>
-                  <div>
-                    {responseError && <p>{responseError}</p>}
-                  </div>
-                  <div>
-                    {responseStream && (
-                      <div style={{textAlign: "left"}}>
-                        {responseStream.map(({ stream, options, instructions }) => (
-                          <div>
-                            <ul>
-                              <li><a style={{fontWeight: 'bold'}}>{stream}</a>: {instructions}</li>
-                              <ul>
-                                {options.map(o => (
-                                  <li>{o}</li>
-                                ))}
-                              </ul>
-                            </ul>
-                          </div>
-                        ))}
+            <div>
+              <div className="image-preview">
+                <h3>Selected Image:</h3>
+                <img src={selectedImage.url} alt="Selected" />
+              </div>
+              <div>
+              <ToggleButtonGroup
+                value={disposeMode}
+                exclusive
+                onChange={handleDisposeModeChange}
+              >
+                <ToggleButton value="donate">
+                  {"Donate"}
+                </ToggleButton>
+                <ToggleButton value="waste">
+                  {"Dispose"}
+                </ToggleButton>
+              </ToggleButtonGroup>
+              </div>
+            </div>
+          )}
+          {loading ? (
+            <CircularProgress style={{ marginTop: 10 }} />
+          ) : (
+            <div>
+              <div>
+                {responseError && <p>{responseError}</p>}
+              </div>
+              <div>
+                {responseStream && (
+                  <div style={{textAlign: "left"}}>
+                    {responseStream.map(({ stream, options, instructions }) => (
+                      <div>
+                        <ul>
+                          <li><a style={{fontWeight: 'bold'}}>{stream}</a>: {instructions}</li>
+                          <ul>
+                            {options.map(o => (
+                              <li>{o}</li>
+                            ))}
+                          </ul>
+                        </ul>
                       </div>
-                    )}
+                    ))}
                   </div>
-                </div>
-              )}
+                )}
+              </div>
             </div>
           )}
         </div>
