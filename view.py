4d82df9778a081d819690bbdfca00c86373e15c8
Sam Spilsbury <smspillaz@gmail.com>
2023-11-11 21:32:19 +0200

    view: default port to 8080

--- view.py
+++ view.py
@@ -45,5 +45,5 @@ if __name__ == "__main__":
     with open("api_key.txt") as f:
         api_key = f.read().strip()
 
-    port = int(os.environ.get('PORT', 5555))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
